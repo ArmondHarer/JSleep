e492b26828fb2b09ce3f95ab02cfbc05956a906d
unknown <armond.harer@ui.ac.id>
2022-09-13 20:17:04 +0700

    Preparing Git in local repository, please wait...

---
+++ Porgram Kalkulator/kalkulatorSederhana.java
@@ -0,0 +1,15 @@
+
+/**
+ * Simple calculator that add, subtract, multiply and divide two variable
+ *
+ * @AH (your name)
+ * @7/9/22, version 1.1
+ */
+public class kalkulatorSederhana
+{
+
+    public static void main (String[] args)
+    { 
+        
+    }
+}
