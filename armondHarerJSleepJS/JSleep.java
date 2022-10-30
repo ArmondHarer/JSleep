3ffec4fbd6ffdb2f526cb62fbcf20e2e572be268
Armond Harer Armond Harer <armond.harer@ui.ac.id>
2022-10-30 11:31:15 +0700

    TP Modul 6

--- armondHarerJSleepJS/JSleep.java
+++ armondHarerJSleepJS/JSleep.java
@@ -1,5 +1,41 @@
 package armondHarerJSleepJS;
-import java.sql.Date;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
+
+public class JSleep
+{
+	class Country{
+		public String name;
+		public int population;
+		public List<String> listOfStates;
+	}
+	
+	public static void main (String[] args)
+	{
+		String filepath= "C:\\Users\\armon\\File Coolyeah Armond\\OOP\\Porgram Armond\\armondHarerJSleepJS\\city.json";
+		Gson gson = new Gson();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filepath));
+			Country input = gson.fromJson(br, Country.class);
+			System.out.println("name: " + input.name);
+			System.out.println("population: " + input.population);
+			System.out.println("states :");
+			input.listOfStates.forEach(state -> System.out.println(state));
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
+/*import java.sql.Date;
 
 public class JSleep
 {
@@ -70,7 +106,6 @@ public class JSleep
         System.out.println(test.price.price);
     }
     */
-}
 
 /*
  *
